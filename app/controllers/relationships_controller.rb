53cc0fd91da1a5464e6056d9b79f0f8b1fc6cd70
EC2 Default User <ec2-user@ip-172-31-8-73.ap-northeast-1.compute.internal>
2023-02-02 13:17:40 +0000

    課題4(フォロー/フォロワー機能 作業中)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,5 @@
 class RelationshipsController < ApplicationController
   
-  def followers
-    @user = User.find(params[:user_id])
-    @following_users = @user.followers
-  end
-  
   def create
     current_user.follow(params[:user_id])
     redirect_to request.referer
@@ -13,6 +8,16 @@ class RelationshipsController < ApplicationController
   def destroy
     current_user.unfollow(params[:user_id])
     redirect_to request.referer  
+  end
+  
+  def followers
+    @user = User.find(params[:user_id])
+    @follower_users = @user.followers
+  end
+  
+  def followings
+    @user = User.find(params[:user_id])
+    @following_users = @user.followings
   end
   
 end
