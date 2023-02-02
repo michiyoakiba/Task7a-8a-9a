7d7c4d7520e29313528c80108a6740369b89941d
EC2 Default User <ec2-user@ip-172-31-8-73.ap-northeast-1.compute.internal>
2023-02-02 05:40:47 +0000

    課題4(フォロー/フォロワー機能 作業中)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
     @books = Book.all
     @book = Book.new
   end
@@ -25,7 +27,15 @@ class UsersController < ApplicationController
     end
   end
   
-  
+  def follows
+    user = User.find(params[:id])
+    @users = user.following_user.page(params[:page]).per(3).reverse_order
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.follower_user.page(params[:page]).per(3).reverse_order
+  end
   
   private
 
