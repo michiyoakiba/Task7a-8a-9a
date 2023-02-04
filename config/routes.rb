06d1139a83700cb704f56333b9bcf04875b8ec89
EC2 Default User <ec2-user@ip-172-31-8-73.ap-northeast-1.compute.internal>
2023-02-04 08:08:53 +0000

    課題5(検索機能 作業中)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
   resources :users, only: [:index,:show,:edit,:update]
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
