9281a16edfe2bea65899c1bcae3b47cfae319293
michiyoakiba <119918454+michiyoakiba@users.noreply.github.com>
2023-02-07 08:50:40 +0000

    課題6(実装した機能の非同期通信化 完成)

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,21 @@
 class BookCommentsController < ApplicationController
-    
+  before_action :authenticate_user!
+  
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    comment.book_id = @book.id
+    unless comment.save
+    # redirect_to book_path(book)
+      render 'error'
     end
+  end
   
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
